fd21154ca83f729bab45cebb12414ac190055ae2
RodrigoVitor <rodrigovitorsc2001@gmail.com>
2021-03-18 15:52:09 -0300

    Funcionalidades

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
 let hour = "00"
-let minutes = "00"
+let minutes = "00" 
 let second = "00"
 let run = document.getElementById('run')
+let tmp;
 
-
-run.innerHTML = "<span>" + hour + "</span> <span>" + minutes + "</span> <span>" + second + "</span>"
 //start
 function start()
 {
     
-    setInterval(() => {
+     tmp = setInterval(() => {
         second++ 
         //verificar se o segundo é menor que 10
         if(second < 10)
@@ -19,12 +18,30 @@ function start()
         //mostrar o minuto quando segundo atingir 1 minuto
         if(second > 60)
         {
-            
+            second = "00"
+            minutes++
+            //se minutes < 10
+            if(minutes < 10)
+            {
+                minutes = "0" + minutes
+            }
+        }
+        //mostrar a hora quando o minuto chegar em 60
+        if(minutes > 60)
+        {
+            second = "00"
+            minutes = "50"
+            hour++
+            //verficar se hora é menor que 10
+            if(hour < 10)
+            {
+                hour = "0" + hour
+            }
         }
         //mostrar funcionando
         run.innerHTML = "<span>" + hour + "</span> <span>" + minutes + "</span> <span>" + second + "</span>" 
 
-    },100)
+    },1000)
 
     
 }
@@ -32,14 +49,24 @@ function start()
 //pause
 function pause()
 {
-
+    clearInterval(tmp)
 }
 
 //reset
 function reset()
 {
-
+    //parar função
+    clearInterval(tmp)
+    //zerar tudo
+    hour = "00"
+    minutes = "00"
+    second = "00"
+    //mostrar zerado
+    run.innerHTML = "<span>" + hour + "</span> <span>" + minutes + "</span> <span>" + second + "</span>"
 }
+
+// mostrar o tempo sem nenhuma função
+run.innerHTML = "<span>" + hour + "</span> <span>" + minutes + "</span> <span>" + second + "</span>"
 
 
 
